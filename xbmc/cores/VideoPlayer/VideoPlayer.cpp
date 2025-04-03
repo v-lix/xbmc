cfb0cec19950487b46e318e459a3b786cec22d75
Portisch <hugo.portisch@yahoo.de>
2025-04-03 13:22:49 +0200

    VideoPlayer: fix handling of seek offset Do not set seek offset when chapter seek did happen. Do not clear out seek offset when seek did happen.

--- xbmc/cores/VideoPlayer/VideoPlayer.cpp
+++ xbmc/cores/VideoPlayer/VideoPlayer.cpp
@@ -3003,7 +3003,6 @@ void CVideoPlayer::HandleMessages()
           m_callback.OnPlayBackSeekChapter(msg.GetChapter());
         }
       }
-      m_processInfo->SeekFinished(offset);
     }
     else if (pMsg->IsType(CDVDMsg::DEMUXER_RESET))
     {
@@ -3716,7 +3715,7 @@ bool CVideoPlayer::SeekTimeRelative(int64_t iTime)
   mode.relative = true;
   mode.backward = (iTime < 0) ? true : false;
   mode.accurate = false;
-  mode.trickplay = false;
+  mode.trickplay = true;
   mode.sync = true;
 
   m_messenger.Put(std::make_shared<CDVDMsgPlayerSeek>(mode));
