764b707296ce3bda70a7529b48a7c4d98736a0f5
pannal <1359593+pannal@users.noreply.github.com>
2026-05-28 22:09:36 +0200

    Revert "AMLCodec: minimal vmap for non-interlace DV"
    This reverts commit 56e7abff2ef14211fd539a755e716731aac75609.

--- xbmc/cores/VideoPlayer/DVDCodecs/Video/AMLCodec.h
+++ xbmc/cores/VideoPlayer/DVDCodecs/Video/AMLCodec.h
@@ -129,7 +129,6 @@ private:
 
   PosixFilePtr     m_amlVideoFile;
   std::string      m_defaultVfmMap;
-  std::string      m_dvblpathVfmMap;
 
   static std::atomic_flag  m_pollSync;
   static int m_pollDevice;
